c51215110ba40e93a0f8379a625a435c63372ba8
Kapil Rathore <116137491+rathorekapil20@users.noreply.github.com>
2023-08-28 22:24:23 +0530

    header

--- js/main.js
+++ js/main.js
@@ -11,6 +11,14 @@
     };
     spinner();
     
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.navbar').addClass('sticky-top shadow-sm');
+        } else {
+            $('.navbar').removeClass('sticky-top shadow-sm');
+        }
+    });
     
     // Initiate the wowjs
     new WOW().init();
